28b9614ffa6f3df1f66d3e7708b01c5c946f45ad
zhangsean <zxf2342@qq.com>
2021-04-27 23:52:20 +0800

    修正acmd获取证书端口绑定

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-acme.sh --issue --standalone -d $DOMAIN
+acme.sh --issue --standalone -d $DOMAIN --httpport $PORT --tlsport $PORT
 for f in $(ls -1 /var/www/*.json); do envsubst < $f > $f; done
 envsubst < /etc/xray/config.json > /etc/xray/config.json
 nginx
