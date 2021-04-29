c802d073fc33effca02e4a3b4bc057e8f10490ff
zhangsean <zxf2342@qq.com>
2021-04-29 17:20:03 +0800

    通过CF KV暂存证书

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,10 @@ FROM teddysun/xray
 RUN apk add --no-cache acme.sh socat gettext nginx \
  && mkdir /run/nginx \
  && sed -ie 's/80/18080/g; 10 d; 9 a root /var/www;\nautoindex on;' /etc/nginx/conf.d/default.conf \
- && echo "Hello World" > /var/www/index.html
+ && echo "Hello World" > /var/www/index.html \
+ && curl -sSL https://github.com/cloudflare/wrangler/releases/download/v1.16.1/wrangler-v1.16.1-x86_64-unknown-linux-musl.tar.gz | tar zxf - \
+ && mv dist/wrangler /usr/bin/ \
+ && rm -rf dist/
 ADD config_client /var/www
 ADD config.json /etc/xray/config.json
 ADD entrypoint.sh /
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-acme.sh --issue --standalone -d $DOMAIN --httpport $PORT --tlsport $PORT
+wrangler init
+echo $CF_KV_API_TOKEN | wrangler config
+ACCOUNT_ID=`wrangler whoami | sed -n '/s Account/s/.*Account | \(.*\) |/\1/p'`
+sed -i "/account_id/s/''/'$ACCOUNT_ID'/" wrangler.toml
+wrangler kv:key get cer -n $CF_KV_NAMESPACE_ID > ssl.cer
+if grep 'key not found' ssl.cer; then
+    acme.sh --issue --standalone -d $DOMAIN --httpport $PORT --tlsport $PORT
+    ln -s ~/.acme.sh/$DOMAIN/fullchain.cer ssl.cer
+    ln -s ~/.acme.sh/$DOMAIN/$DOMAIN.key ssl.key
+    wrangler kv:key put -n 4d7b910f88a846329264bf31cc3f45db cer ssl.cer --path
+    wrangler kv:key put -n 4d7b910f88a846329264bf31cc3f45db key ssl.key --path
+else
+    wrangler kv:key get key -n $CF_KV_NAMESPACE_ID > ssl.cer
+fi
 for f in $(ls -1 /var/www/*.json); do envsubst < $f > $f; done
 envsubst < /etc/xray/config.json > /etc/xray/config.json
 nginx
